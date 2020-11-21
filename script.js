66ba9052dbcd04a437ae0b5b404506f884644da8
Ronak Vanpariya <vanpariyar@gmail.com>
2020-11-22 00:37:09 +0530

    Added new fields

--- script.js
+++ script.js
@@ -6,23 +6,20 @@ function authUser (){
     (localStorage.getItem('login') == '0') && (window.location.href = '/login/');   
 }
 authUser();
-$(document).ready(function(){
-    //Prevent submit to form of user
-    // $('#quiz-form').submit(function(e){
-    //     e.preventDefault();        
-    // });
 
-    /**           
-     * @param {type} type number
-     * @param {data} data JSON
-     */
-    function setItem( type , data ){
+/**           
+ * @param {type} type number
+ * @param {data} data JSON
+ */
+
+class Quiz {
+    setItem( type , data ){
         switch(type){
-            case 1:
+            case 'quiz':
                 localStorage.setItem('quiz', JSON.stringify(data));
                 return true;
 
-            case 2:
+            case 'questionIndex':
                 if(localStorage.getItem('questionIndex')){
                     localStorage.setItem('questionIndex', data);
                     return true;
@@ -30,7 +27,7 @@ $(document).ready(function(){
                     localStorage.setItem('questionIndex', '0')
                     return true;
                 }    
-                case 3:
+                case 'userAnswer':
                     if(localStorage.getItem('userAnswer')){
                         localStorage.setItem('userAnswer', JSON.stringify(data));
                         return true;
@@ -42,19 +39,19 @@ $(document).ready(function(){
                 return false;
         }
     }
-    function getItem( type = 0 ){
+    getItem( type = 0 ){
         switch(type){
-            case 1:
+            case 'quiz':
                 return JSON.parse(localStorage.getItem('quiz'));
 
-            case 2:
+            case 'questionIndex':
                 if(localStorage.getItem('questionIndex')){
                     return parseInt(localStorage.getItem('questionIndex'));
                 }else{
                     localStorage.setItem('questionIndex', '0')
                     return parseInt(localStorage.getItem('questionIndex'));
                 }
-            case 3:
+            case 'userAnswer':
                 if(localStorage.getItem('userAnswer')){
                     return JSON.parse(localStorage.getItem('userAnswer'));
                 }else{
@@ -62,7 +59,7 @@ $(document).ready(function(){
                     return JSON.parse(localStorage.getItem('userAnswer'));
                 }
                 
-            case 4:
+            case 'token':
                 if(localStorage.getItem('token')){
                     return localStorage.getItem('token');
                 }else{
@@ -75,12 +72,12 @@ $(document).ready(function(){
         }
     }
 
-    function removeItem( type ){
+    removeItem( type ){
         switch (type) {
-            case 1:
+            case 'quiz':
                 return localStorage.removeItem('quiz');
 
-            case 2:
+            case 'questionIndex':
                 localStorage.removeItem('questionIndex');
                 break;
         
@@ -88,140 +85,140 @@ $(document).ready(function(){
                 break;
         }
     }
-    
-    function increaseAnsIndex(questionIndex){
-        var quiz = getItem(1);
-        if(!(questionIndex+1 >= quiz.length) ){
-            questionIndex++;
-            setItem(2,questionIndex); 
-            return questionIndex;  
+}
+
+function increaseAnsIndex(questionIndex){
+    var quiz = getItem('');
+    if(!(questionIndex+1 >= quiz.length) ){
+        questionIndex++;
+        setItem(2,questionIndex); 
+        return questionIndex;  
+    }
+    return false;
+}
+
+/**
+ *  Common Function  For send data via ajax.
+ *  Send the data To doPost() appScript.
+ */
+function qfsSendData( data = {} ) {
+    $.ajax({
+        type: "POST",
+        url: scriptURL,
+        data: data,
+        headers: {
+            'Content-type': 'application/x-www-form-urlencoded',
+        },
+        success: function (data)
+        {
+            alert(data);
         }
-        return false;
-    }
+    });
+}
 
-    /**
-     *  Common Function  For send data via ajax.
-     *  Send the data To doPost() appScript.
-     */
-    function qfsSendData( data = {} ) {
-        $.ajax({
-            type: "POST",
-            url: scriptURL,
-            data: data,
-            headers: {
-                'Content-type': 'application/x-www-form-urlencoded',
-            },
-            success: function (data)
-            {
-                alert(data);
-            }
-        });
-    }
+/**
+ * Show the result After Quiz answer done.
+ * get the data from Local Storage
+ */
 
-    /**
-     * Show the result After Quiz answer done.
-     * get the data from Local Storage
-     */
-
-    function ShowResult(){
-        var rightAnswer = [];
-        var quiz = getItem(1);
-        quiz.map(function(value){
-            rightAnswer.push(value.RightAnswer);
-        });
-        var totalScore = 0;
-        let userAnswers = getItem(3);
-        rightAnswer.map(function(answer, key){
-            if(answer == userAnswers[key]){
-                totalScore++;
-            }
-        });
-        percentage =  ((totalScore / rightAnswer.length)*100).toFixed(2) ;
-
-        $(".quiz-body").hide();
-        $("#score").show();
-
-        $("#score h3").text(`Your Total Score is: ${totalScore} out of ${rightAnswer.length} and ${percentage}%`);
-
-        $(".restart-quiz").on('click', function(){
-            $(this).prop('value', 'Loading Your quiz .....')
-            removeItem(2);
-            removeItem(1);
-            var questionIndex = getItem(2);
-            getQuiz();
-            quizSetup(questionIndex);
-            $(".quiz-body").show();
-            $("#score").hide();
-        });
-
-        qfsSendData({ 
-            score: totalScore,
-            action: 'result',
-            token: getItem(4),
-        }); // send data to the s
-    }
-
-    function quizSetup(questionIndex){
-        var quiz = getItem(1);
-        $("#quiz-form input[type=radio]").prop("checked",false);
-        $("#question").text( quiz[questionIndex].question );
-        $("label[for='radio1'] span").text(quiz[questionIndex].answer1);
-        $("label[for='radio2'] span").text(quiz[questionIndex].answer2);
-        $("label[for='radio3'] span").text(quiz[questionIndex].answer3);
-        $("label[for='radio4'] span").text(quiz[questionIndex].answer4);
-        
-        $('.ansBtn').val(questionIndex);
-    }
-
-    /* ---------------------------------------------------------------------------- */
-    if (typeof(Storage) !== "undefined") {
-
-        // NOTE : Remove this if condiotion if found it is for testing purpose
-        function getQuiz(){
-            if(!getItem(1)){
-                $.ajaxSetup({
-                    async: false
-                });
-                $.getJSON( scriptURL+"?action=getquiz", function( data ) {
-                    setItem(1, data);
-                    return data;
-                });
-            }
+function ShowResult(){
+    var rightAnswer = [];
+    var quiz = getItem('');
+    quiz.map(function(value){
+        rightAnswer.push(value.RightAnswer);
+    });
+    var totalScore = 0;
+    let userAnswers = getItem(3);
+    rightAnswer.map(function(answer, key){
+        if(answer == userAnswers[key]){
+            totalScore++;
         }
+    });
+    percentage =  ((totalScore / rightAnswer.length)*100).toFixed(2) ;
+
+    $(".quiz-body").hide();
+    $("#score").show();
+
+    $("#score h3").text(`Your Total Score is: ${totalScore} out of ${rightAnswer.length} and ${percentage}%`);
+
+    $(".restart-quiz").on('click', function(){
+        $(this).prop('value', 'Loading Your quiz .....')
+        removeItem(2);
+        removeItem(1);
+        var questionIndex = getItem(2);
         getQuiz();
-        
-        
-    } else {
-    // Sorry! No Web Storage support..
-    }
-
-    getItem(1);
-    $("#start-btn").click(function(e){
-        $('#rules').hide();
+        quizSetup(questionIndex);
         $(".quiz-body").show();
+        $("#score").hide();
+    });
+
+    qfsSendData({ 
+        score: totalScore,
+        action: 'result',
+        token: getItem(4),
+    }); // send data to the s
+}
+
+function quizSetup(questionIndex){
+    var quiz = getItem('');
+    $("#quiz-form input[type=radio]").prop("checked",false);
+    $("#question").text( quiz[questionIndex].question );
+    $("label[for='radio1'] span").text(quiz[questionIndex].answer1);
+    $("label[for='radio2'] span").text(quiz[questionIndex].answer2);
+    $("label[for='radio3'] span").text(quiz[questionIndex].answer3);
+    $("label[for='radio4'] span").text(quiz[questionIndex].answer4);
+    
+    $('.ansBtn').val(questionIndex);
+}
+
+/* ---------------------------------------------------------------------------- */
+if (typeof(Storage) !== "undefined") {
+
+    // NOTE : Remove this if condiotion if found it is for testing purpose
+    function getQuiz(){
+        if(!getItem('')){
+            $.ajaxSetup({
+                async: false
+            });
+            $.getJSON( scriptURL+"?action=getquiz", function( data ) {
+                setItem(1, data);
+                return data;
+            });
+        }
+    }
+    getQuiz();
+    
+    
+} else {
+// Sorry! No Web Storage support..
+}
+
+getItem('');
+$("#start-btn").click(function(e){
+    $('#rules').hide();
+    $(".quiz-body").show();
+    var questionIndex = getItem(2);
+    var userAnswer = getItem(3);
+    if(userAnswer.length == (questionIndex+1)){
+        ShowResult();
+    }else{
+        quizSetup(questionIndex);
+    }
+});
+
+$('#quiz-form').submit(function(e){
+    e.preventDefault();   
+    if($("#quiz-form input[type=radio]:checked").length == 0){
+        alert('Select The Value Please');
+    }else{
         var questionIndex = getItem(2);
         var userAnswer = getItem(3);
-        if(userAnswer.length == (questionIndex+1)){
-            ShowResult();
-        }else{
+        userAnswer[questionIndex] = (parseInt($('input[name="optradio"]:checked').val()));
+        setItem( 3, userAnswer);
+        if(questionIndex = increaseAnsIndex(questionIndex)){
             quizSetup(questionIndex);
-        }
-    });
-
-    $('#quiz-form').submit(function(e){
-        e.preventDefault();   
-        if($("#quiz-form input[type=radio]:checked").length == 0){
-            alert('Select The Value Please');
         }else{
-            var questionIndex = getItem(2);
-            var userAnswer = getItem(3);
-            userAnswer[questionIndex] = (parseInt($('input[name="optradio"]:checked').val()));
-            setItem( 3, userAnswer);
-            if(questionIndex = increaseAnsIndex(questionIndex)){
-                quizSetup(questionIndex);
-            }else{
-                ShowResult();
-            }
-        }     
-    });
+            ShowResult();
+        }
+    }     
 });
