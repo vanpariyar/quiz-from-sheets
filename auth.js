f23142ea5b63b60e626deb121a884c2cb0b6138f
Ronak Vanpariya <26689210+vanpariyar@users.noreply.github.com>
2022-06-23 16:36:22 +0530

    change

--- auth.js
+++ auth.js
@@ -117,3 +117,20 @@ function onSignIn(googleUser) {
     handleRequest(user, 'login');
 
 }
+
+function gsignin(response) {
+    // decodeJwtResponse() is a custom function defined by you
+    // to decode the credential response.
+    const responsePayload = decodeJwtResponse(response.credential);
+
+    console.log("ID: " + responsePayload.sub);
+    console.log('Full Name: ' + responsePayload.name);
+    console.log('Given Name: ' + responsePayload.given_name);
+    console.log('Family Name: ' + responsePayload.family_name);
+    console.log("Image URL: " + responsePayload.picture);
+    console.log("Email: " + responsePayload.email);
+
+    console.log(user);
+
+    handleRequest(user, 'login');
+ }
